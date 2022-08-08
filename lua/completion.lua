6f8e22d0f0d464c248ba533efb0ea483f71df0ff
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-08-07 18:06:20 -0700

    Fix autocomplete

--- lua/completion.lua
+++ lua/completion.lua
@@ -3,27 +3,14 @@ local function check_back_space()
     return col == 0 or vim.api.nvim_get_current_line():sub(col, col):match('%s')
 end
 
-local function t(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
 function _G.tabmap()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-n>"
+    if vim.fn['coc#pum#visible']() == 1 then
+        return vim.fn['coc#_select_confirm']()
     elseif check_back_space() then
-        return t "<Tab>"
+        return vim.api.nvim_replace_termcodes('<C-g>u<Tab>', true, true, true)
     else
-        vim.fn["coc#refresh"]()
+        return vim.fn['coc#refresh']()
     end
 end
 
-function _G.stabmap()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-p>"
-    else
-        vim.fn["coc#refresh"]()
-    end
-end
-
-vim.keymap.set('i', '<Tab>', 'v:lua.tabmap()', { expr = true })
-vim.keymap.set('i', '<S-Tab>', 'v:lua.stabmap()', { expr = true })
+vim.keymap.set('i', '<Tab>', 'v:lua.tabmap()', {silent = true, expr = true})
