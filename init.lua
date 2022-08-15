06db417bd9c799bfc466c10cb1d4b4dcc08fc8c5
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-08-15 13:50:39 -0700

    Don't load impatient.nvim unless packer is not bootstrapped

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-require('impatient')
 require('plugins')
 require('window')
 require('keybindings')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,8 @@ local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.n
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     Packer_bootstrap = vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd 'packadd packer.nvim'
+else
+    require('impatient')
 end
 
 return require('packer').startup(function(use)
